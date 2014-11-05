8198d5e723b3228d753ec25176437e239f5c5c8b
pomelo-- <pomeloecho@gmail.com>
2014-11-05 22:50:49 +0800

    dadada

--- admin/new_user.php
+++ admin/new_user.php
@@ -93,6 +93,11 @@ $db->close();
 </div>
 -->
 <script src="js/bootstrap.min.js"></script>
+<script src="../Carousel Template for Bootstrap_files/jquery.min.js"></script>
+  <script src="../Carousel Template for Bootstrap_files/bootstrap.min.js"></script>
+  <script src="../Carousel Template for Bootstrap_files/docs.min.js"></script>
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="../Carousel Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
 <div class="navbar-wrapper">
   <div class="container">
 
--- admin/search_user.php
+++ admin/search_user.php
@@ -84,12 +84,58 @@
   <script src="../Carousel Template for Bootstrap_files/docs.min.js"></script>
   <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
   <script src="../Carousel Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
-<div id = "header">
-  <img src = "../images/logo.jpg" height = 100> &nbsp; &nbsp;
-  <font size="25" color="black"><b>BIZ-Tripper Online Booking System</b></font> 
-  <input type="submit" align = "right" onClick="document.location.href = ('../logout.php');" value="logout" name="logout" id="logout" >
-   <input type="submit" align = "right" onClick="document.location.href = ('manage_users.php');" value="back" name="back" id="logout" >
-  <br>
+  
+<div class="navbar-wrapper">
+  <div class="container">
+
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand">BizTripper</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="manage_users.php">Home</a></li>
+
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Manage Users <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="./new_user.php">New user</a></li>
+                <li><a href="./search_user.php">Search user</a></li>
+                <!-- <li><a href="/update_user">Update user</a></li>
+                <li><a href="/delete_user">Delete user</a></li> -->
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="manage_users.php" class="dropdown-toggle" data-toggle="dropdown">Manage Airlines <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="./new_flight.php">New flight</a></li>
+                <li><a href="./manage_flights.php">Search flight</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="http://getbootstrap.com/examples/carousel/#">Separated link</a></li>
+                <li><a href="http://getbootstrap.com/examples/carousel/#">One more separated link</a></li>
+              </ul>
+            </li>
+            <li class="nav navbar-nav navbar-right">
+              <a href="../logout.php">Logout</a>
+            </li>
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+
+  </div>
 </div>
 
 <div id="wrapper">
