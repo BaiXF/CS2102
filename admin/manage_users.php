e719296e63a3da5d05162370ceab16e09904f4cc
debbiecho <bestmosquito@gmail.com>
2014-11-05 16:29:13 +0800

    ok la

--- admin/manage_users.php
+++ admin/manage_users.php
@@ -57,72 +57,155 @@ else{
 	<br /><br /><br />
 -->
 <div class="navbar-wrapper">
-      <div class="container">
+	<div class="container">
 
-        <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand">BizTripper</a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="http://getbootstrap.com/examples/carousel/#">Home</a></li>
+		<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand">BizTripper</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="manage_users.php">Home</a></li>
 
 
-                <li class="dropdown">
-                  <a href="manage_users.php" class="dropdown-toggle" data-toggle="dropdown">Manage Users <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="new_user.php">New user</a></li>
-                    <li><a href="search_user.php">Search user</a></li>
-                    <li><a href="update_user.php">Update user</a></li>
-                    <li><a href="delete_user.php">Delete user</a></li>
-                  </ul>
-                </li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Manage Users <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="/new_user">New user</a></li>
+								<li><a href="/search_user">Search user</a></li>
+								<li><a href="/update_user">Update user</a></li>
+								<li><a href="/delete_user">Delete user</a></li>
+							</ul>
+						</li>
 
-                <li class="dropdown">
-                  <a href="manage_users.php" class="dropdown-toggle" data-toggle="dropdown">Manage Airlines <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Action</a></li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Another action</a></li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">Separated link</a></li>
-                    <li><a href="http://getbootstrap.com/examples/carousel/#">One more separated link</a></li>
-                  </ul>
-                </li>
-                <li class="nav navbar-nav navbar-right">
-                    <a href="http://getbootstrap.com/examples/carousel/#">Logout</a>
-                </li>
-              </ul>
+						<li class="dropdown">
+							<a href="manage_users.php" class="dropdown-toggle" data-toggle="dropdown">Manage Airlines <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="http://getbootstrap.com/examples/carousel/#">Action</a></li>
+								<li><a href="http://getbootstrap.com/examples/carousel/#">Another action</a></li>
+								<li><a href="http://getbootstrap.com/examples/carousel/#">Something else here</a></li>
+								<li class="divider"></li>
+								<li class="dropdown-header">Nav header</li>
+								<li><a href="http://getbootstrap.com/examples/carousel/#">Separated link</a></li>
+								<li><a href="http://getbootstrap.com/examples/carousel/#">One more separated link</a></li>
+							</ul>
+						</li>
+						<li class="nav navbar-nav navbar-right">
+							<a href="http://getbootstrap.com/examples/carousel/#">Logout</a>
+						</li>
+					</ul>
 
-            </div>
+				</div>
 
-          </div>
-        </nav>
+			</div>
+		</nav>
 
-      </div>
+	</div>
+</div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class=""></li>
+		<li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner" role="listbox">
+		<div class="item">
+			<img src="../pic/BusinessStrategyChess.jpg" alt="First slide">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Welcome to Biz Tripper Admin Portal</h1>
+					<p>
+						The most efficient business trip management exclusively designed for your company. The first service provider for professional business booking. 
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="item active">
+			<img src="../pic/plane.jpg" alt="Second slide">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Manage flights have never been easier</h1>
+					<p>
+						Built in database of flight information. Identify the right flight the fastest and the safest. Simple interface for you to manage flight information. 
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<img src="../pic/businesstrip.jpg" alt="Third slide">
+			<div class="container">
+				<div class="carousel-caption">
+					<h1>Give your employees the superior business trip experience</h1>
+					<p>
+						Manage every employee details and book exactly the flight they want for the next business trip.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="../pic/business-man.png" style="width: 140px; height: 140px;">
+          <h2>Meet Dexter</h2>
+          <p>Biz tripper is one of the best flight booking system I have ever used. It is so convenient that I can even book my flight the last minute.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="../pic/black.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <h2>Meet Michael</h2>
+          <p>I am impressed with the simple design of Biz Tripper. In the past we used to hire a secretary to book flight for our emloyees, now it is all handled by Biz Tripper.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="../pic/woman.jpg" style="width: 140px; height: 140px;">
+          <h2>Meet Sharon</h2>
+          <p>Biz Tripper keeps you from the excess information and only offers you what is the most crucial. </p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+      <!-- FOOTER -->
+      <footer>
+        <p>© 2014 BizTripper, Inc.</p>
+        <!--<p>Created by Bai Xuefeng, Liu Chuning and Sun Jingwen :)</p>-->
+      </footer>
+
     </div>
-	<div id = "sqlquery">
-		<form action="" method="post">
-			<table align = "center">
-				<tr>	
-					<th><p><h3><center>Write your single line SQL query to quick access the database.<br> Submit with Enter Key</center></h3></p></th>
-				</tr>
-				<tr>
-					<th><input style="height:30px;width: 900px;font-size:14pt;" id = "box" name="query" type="text"></th>
-				</tr>
-				<tr>
-					<th>
-						<br/>
-						<br/>
-						<?php
+
+
+<div id = "sqlquery">
+	<form action="" method="post">
+		<table align = "center">
+			<tr>	
+				<th><p><h3><center>Write your single line SQL query to quick access the database.<br> Submit with Enter Key</center></h3></p></th>
+			</tr>
+			<tr>
+				<th><input style="height:30px;width: 900px;font-size:14pt;" id = "box" name="query" type="text"></th>
+			</tr>
+			<tr>
+				<th>
+					<br/>
+					<br/>
+					<?php
 					global $errorMsg, $result;
 					if ($errorMsg !=""){
 						echo $errorMsg;
@@ -150,10 +233,10 @@ else{
 			</table> 
 		</form>
 	</div>
-	<script src="./Carousel Template for Bootstrap_files/jquery.min.js"></script>
-	<script src="./Carousel Template for Bootstrap_files/bootstrap.min.js"></script>
-	<script src="./Carousel Template for Bootstrap_files/docs.min.js"></script>
+	<script src="../Carousel Template for Bootstrap_files/jquery.min.js"></script>
+	<script src="../Carousel Template for Bootstrap_files/bootstrap.min.js"></script>
+	<script src="../Carousel Template for Bootstrap_files/docs.min.js"></script>
 	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	<script src="./Carousel Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+	<script src="../Carousel Template for Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
